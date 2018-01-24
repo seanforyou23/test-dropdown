f4f9bb5148f4958e50b2cd01c79bbb9d836ccfc9
Michael Spaxman <seanforyou23@gmail.com>
2018-01-24 14:23:11 -0500

    add a bunch more dependencies and register them in main app module, looking for errors, still none found

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,8 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { HttpModule } from '@angular/http';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { DynamicFormsCoreModule } from '@ng-dynamic-forms/core';
 import {
   AlertModule,
   BsDropdownModule,
@@ -10,6 +13,9 @@ import {
   TooltipModule,
   TypeaheadModule
 } from 'ngx-bootstrap';
+import { TagInputModule } from 'ngx-chips';
+import { EffectsModule } from '@ngrx/effects';
+import { TourNgxBootstrapModule } from 'ngx-tour-ngx-bootstrap';
 import { NotificationModule } from 'patternfly-ng';
 import { AppComponent } from './app.component';
 
@@ -19,6 +25,10 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
+    FormsModule,
+    ReactiveFormsModule,
+    HttpModule,
+    DynamicFormsCoreModule.forRoot(),
     BsDropdownModule.forRoot(),
     AlertModule.forRoot(),
     CollapseModule.forRoot(),
@@ -27,6 +37,9 @@ import { AppComponent } from './app.component';
     TabsModule.forRoot(),
     TooltipModule.forRoot(),
     TypeaheadModule.forRoot(),
+    TagInputModule,
+    EffectsModule.forRoot([]),
+    TourNgxBootstrapModule.forRoot(),
     NotificationModule
   ],
   providers: [],
