4409bb24037dd39bdd05b6c6a7ad7ef2d7ca98cc
Michael Spaxman <seanforyou23@gmail.com>
2018-01-25 14:22:55 -0500

    add restangular, pluralize, store, and typings.d.ts, still no issue with building

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,5 @@
 import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import { NgModule, APP_INITIALIZER, InjectionToken } from '@angular/core';
 import { HttpModule } from '@angular/http';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { DynamicFormsCoreModule } from '@ng-dynamic-forms/core';
@@ -14,12 +14,56 @@ import {
   TypeaheadModule
 } from 'ngx-bootstrap';
 import { TagInputModule } from 'ngx-chips';
-
+import { StoreModule } from '@ngrx/store';
 // import { EffectsModule } from '@ngrx/effects';
 import { TourNgxBootstrapModule } from 'ngx-tour-ngx-bootstrap';
+import { Restangular, RestangularModule } from 'ngx-restangular';
 import { NotificationModule } from 'patternfly-ng';
+
+import { appConfigInitializer, ConfigService } from './config.service';
+import { StoreModule as LegacyStoreModule } from './store/store.module';
 import { ApiModule } from './api';
+import { platformReducer, SYNDESIS_GUARDS } from './platform';
 import { AppComponent } from './app.component';
+
+export function restangularProviderConfigurer(
+  restangularProvider: any,
+  config: ConfigService
+) {
+  restangularProvider.setPlainByDefault(true);
+  restangularProvider.setBaseUrl(config.getSettings().apiEndpoint);
+
+  restangularProvider.addResponseInterceptor((data: any, operation: string) => {
+    if (operation === 'getList' && data && Array.isArray(data.items)) {
+      const pagingData = data.items;
+      if (!!pagingData.totalCount) {
+        pagingData.totalCount = data.totalCount;
+      } else {
+        pagingData.totalCount = pagingData.length;
+      }
+      return pagingData;
+    }
+    if (!data) {
+      return [];
+    }
+    return data;
+  });
+}
+
+export const RESTANGULAR_MAPPER = new InjectionToken<Restangular>(
+  'restangularMapper'
+);
+export function mapperRestangularProvider(
+  restangular: Restangular,
+  config: ConfigService
+) {
+  return restangular.withConfig(restangularConfigurer => {
+    const mapperEndpoint = config.getSettings().mapperEndpoint;
+    restangularConfigurer.setBaseUrl(
+      mapperEndpoint ? mapperEndpoint : '/mapper/v1'
+    );
+  });
+}
 
 @NgModule({
   declarations: [
@@ -43,9 +87,25 @@ import { AppComponent } from './app.component';
     // EffectsModule.forRoot([]), // gives runtime error "Uncaught Error: StaticInjectorError[t -> e]:..."
     ApiModule.forRoot(),
     TourNgxBootstrapModule.forRoot(),
+    LegacyStoreModule,
+    StoreModule.forRoot(platformReducer),
     NotificationModule
   ],
-  providers: [],
+  providers: [
+    ...SYNDESIS_GUARDS,
+    {
+      provide: APP_INITIALIZER,
+      useFactory: appConfigInitializer,
+      deps: [ConfigService],
+      multi: true
+    },
+    {
+      provide: RESTANGULAR_MAPPER,
+      useFactory: mapperRestangularProvider,
+      deps: [Restangular, ConfigService]
+    },
+    ConfigService
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
--- src/typings.d.ts
+++ src/typings.d.ts
@@ -3,3 +3,35 @@ declare var module: NodeModule;
 interface NodeModule {
   id: string;
 }
+
+
+// Typings reference file, you can add your own global typings here
+// https://www.typescriptlang.org/docs/handbook/writing-declaration-files.html
+
+interface EventSourceInit {
+  withCredentials?: boolean;
+}
+
+declare class EventSource {
+  url: string;
+  withCredentials: boolean;
+  CONNECTING: number;
+  OPEN: number;
+  CLOSED: number;
+  readyState: number;
+
+  onmessage: (event: any) => void;
+
+  close: () => void;
+
+  constructor(url: string, eventSourceInitDict?: EventSourceInit);
+
+  addEventListener(event: string, cb: (event: any) => void);
+
+  removeEventListener(event: string, cb: (event: any) => void);
+}
+
+// add type definition for https://github.com/liabru/jquery-match-height plugin
+interface JQuery {
+  matchHeight(options?: any): any;
+}
